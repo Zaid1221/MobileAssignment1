65c37da0995257deab442f11a2ff882caec10bad
Zaid1221 <zaid.javaid@gmail.com>
2017-09-11 00:32:30 -0400

    Made a button

--- app/src/main/java/com/example/zaid/mobileassignment1/MainActivity.java
+++ app/src/main/java/com/example/zaid/mobileassignment1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.zaid.mobileassignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Log.i("Info", "Button Clicked");
     }
 }
