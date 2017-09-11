57ecaa630c7324d98aed4a0fe18d09c0acd8b068
Zaid1221 <zaid.javaid@gmail.com>
2017-09-11 01:51:57 -0400

    Toast

--- app/src/main/java/com/example/zaid/mobileassignment1/MainActivity.java
+++ app/src/main/java/com/example/zaid/mobileassignment1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
-        Log.i("Info", "Button Clicked");
+        Toast.makeText(MainActivity.this, "1", Toast.LENGTH_LONG).show();
     }
 }
